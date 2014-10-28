5bd9b601c042a992b0b1965c760bc39ce63f4ac7
Gavin Lloyd <gavinhungry@gmail.com>
2014-10-28 14:05:29 -0700

    Add actions (create, open, close)

--- skul.sh
+++ skul.sh
@@ -21,7 +21,7 @@ ZERO=/dev/zero
 CIPHER=aes-xts-plain64
 KEYSIZE=256
 HASH=sha512
-ITER=2000
+ITER=4000
 
 MAPPER=/dev/mapper
 
@@ -63,59 +63,95 @@ clean() {
   echo $1 | sed -e 's/[^[:alnum:]]/_/g' | tr -s '_' | tr A-Z a-z
 }
 
-$SU -v || exit 1
+checksu() {
+  $SU -v || exit 1
+}
+
+create() {
+  checksu
+  SIZE=$1
+  KEY=$2
+
+  [ -e $CONTAINER ] && error "Container '$CONTAINER' already exists"
+  [ -b $MAP ] && error "$MAP is already mapped"
+  [ "$KEY" == "$CONTAINER" ] && error 'Key and container cannot be the same file'
+  [[ "$KEY" && ( ! -f "$KEY" || ! -r "$KEY" ) ]] && error 'Cannot read key'
+
+  inform "Using $CIPHER ${KEYSIZE}-bit $HASH"
+  inform "Creating container '$CONTAINER'"
+  $DD if=$ZERO of=$CONTAINER bs=1M count=$SIZE
+  [ $? -eq 0 ] || error 'Error creating container'
+
+  inform "Encrypting container '$CONTAINER'"
+  if [ -n "$KEY" ]; then
+    $SU $CS luksFormat $CONTAINER -c $CIPHER -s $KEYSIZE -h $HASH -i $ITER -d "$KEY"
+  else
+    $SU $CS luksFormat $CONTAINER -c $CIPHER -s $KEYSIZE -h $HASH -i $ITER
+  fi
+  [ $? -eq 0 ] || error 'Error encrypting container'
+
+  open $KEY
+  wipe
+  mount
+}
+
+open() {
+  checksu
+  KEY=$1
+
+  inform "Opening container '$CONTAINER'"
+  if [ -n "$KEY" ]; then
+    $SU $CS luksOpen $CONTAINER $MAPID -d "$KEY"
+  else
+    $SU $CS luksOpen $CONTAINER $MAPID
+  fi
+  [ $? -eq 0 ] || error 'Error opening container'
+}
+
+mount() {
+  checksu
+
+  inform "Mounting '$MAPID'"
+  $SU $UDISKS --mount $MAP
+  [ $? -eq 0 ] || error 'Error mounting'
+
+  MOUNTPOINT=$($UDISKS --show-info $MAP | grep 'mount paths:' | sed s/^\ *mount.paths:\ *//g)
+  inform "Setting mountpoint permissions on '$MOUNTPOINT'"
+  USER=$(id -u -n)
+  GROUP=$(id -g -n)
+  $SU $CHOWN $USER:$GROUP $MOUNTPOINT
+}
+
+wipe() {
+  checksu
+
+  inform "Writing encrypted zeroes to '$MAPID'"
+  $SU $DD if=$ZERO of=$MAP bs=1M
+  # [ $? -eq 0 ] || error 'Error writing encrypted zeros'
+
+  inform "Creating filesytem on '$MAPID'"
+  $SU $EXT4 $MAP -L $MAPID
+  [ $? -eq 0 ] || error 'Error creating filesystem'
+}
+
+close() {
+  checksu
+
+  $SU $UDISKS --unmount $MAP &> /dev/null
+  $SU $CS luksClose $MAP
+}
 
 if [ $# -lt 2 ]; then
-  usage 'FILENAME SIZE [KEYFILE]'
+  usage '[create|open|close] FILENAME [SIZE] [KEYFILE]'
   exit 1
 fi
 
-CONTAIN=$1
-SIZE=$2
-KEY=$3
-
-MAPID="skul-$(clean $CONTAIN)"
+CONTAINER=$2
+MAPID="skul-$(clean $CONTAINER)"
 MAP=$MAPPER/$MAPID
 
-[ -e $CONTAIN ] && error "Container '${CONTAIN}' already exists"
-[ -b $MAP ] && error "$MAP is already mapped"
-[ "$KEY" == "$CONTAIN" ] && error 'Key and container cannot be the same file'
-[[ "$KEY" && ( ! -f "$KEY" || ! -r "$KEY" ) ]] && error 'Cannot read key'
-
-inform "Creating container '$CONTAIN'"
-$DD if=$ZERO of=$CONTAIN bs=1M count=$SIZE
-[ $? -eq 0 ] || error 'Error creating container'
-
-inform "Encrypting container '$CONTAIN'"
-if [ -n "$KEY" ]; then
-  $SU $CS luksFormat $CONTAIN -c $CIPHER -s $KEYSIZE -h $HASH -i $ITER -d "$KEY"
-else
-  $SU $CS luksFormat $CONTAIN -c $CIPHER -s $KEYSIZE -h $HASH -i $ITER
-fi
-[ $? -eq 0 ] || error 'Error encrypting container'
-
-inform "Opening container '$CONTAIN'"
-if [ -n "$KEY" ]; then
-  $SU $CS luksOpen $CONTAIN $MAPID -d "$KEY"
-else
-  $SU $CS luksOpen $CONTAIN $MAPID
-fi
-[ $? -eq 0 ] || error 'Error opening container'
-
-inform "Writing encrypted zeroes to '$MAPID'"
-$SU $DD if=$ZERO of=$MAP bs=1M
-# [ $? -eq 0 ] || error 'Error writing encrypted zeros'
-
-inform "Creating filesytem on '$MAPID'"
-$SU $EXT4 $MAP -L $MAPID
-[ $? -eq 0 ] || error 'Error creating filesystem'
-
-inform "Mounting '$MAPID'"
-$SU $UDISKS --mount $MAP
-[ $? -eq 0 ] || error 'Error mounting'
-
-MOUNTPOINT=$($UDISKS --show-info $MAP | grep 'mount paths:' | sed s/^\ *mount.paths:\ *//g)
-inform "Setting mountpoint permissions on '$MOUNTPOINT'"
-USER=$(id -u -n)
-GROUP=$(id -g -n)
-$SU $CHOWN $USER:$GROUP $MOUNTPOINT
+case $1 in
+  'create') create $3 $4 ;;
+  'open') open $3; mount $3 ;;
+  'close') close ;;
+esac
